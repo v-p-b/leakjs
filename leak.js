0832070b03daf147c438945095721bfadc7f5fd7
b <vpbalint@silentsignal.hu>
2019-08-21 13:15:17 +0200

    Initial commit based on sample upload code

---
+++ leak.js
@@ -0,0 +1,31 @@
+var http = require('http');
+var formidable = require('formidable');
+var fs = require('fs');
+
+http.createServer(function(req, res) {
+
+    // File upload from https://www.w3schools.com/nodejs/nodejs_uploadfiles.asp   
+    if (req.url == '/fileupload') {
+        var form = new formidable.IncomingForm();
+        form.parse(req, function(err, fields, files) {
+            var oldpath = files.filetoupload.path;
+            var newpath = '/app/instance/uploads/' + files.filetoupload.name; // Unsanitized!
+            fs.rename(oldpath, newpath, function(err) {
+                if (err) throw err;
+                res.write('File uploaded and moved!');
+                res.end();
+            });
+        });
+    } else {
+        res.writeHead(200, {
+            'Content-Type': 'text/html'
+        });
+        res.write('<form action="fileupload" method="post" enctype="multipart/form-data">');
+        res.write('<input type="file" name="filetoupload"><br>');
+        res.write('<input type="submit">');
+        res.write('</form>');
+        return res.end();
+    }
+
+
+}).listen(8099);
